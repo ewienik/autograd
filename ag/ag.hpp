66055729e83119fc383ed394dc499b3b266bcc2f
Pawel Pery <pawel.pery@eservice.com.pl>
2021-02-05 23:18:26 +0100

    Add tests from reqs

--- ag/ag.hpp
+++ ag/ag.hpp
@@ -9,31 +9,31 @@
 #include <ag/variable.hpp>
 
 template <int N, int M, typename O>
-auto avg(ag::Variable<N, M, O>& variable) {
+auto avg(ag::Variable<N, M, O> const& variable) {
     using Operation = ag::operation::Avg<N, M, O>;
     return ag::Variable<1, 1, Operation>{Operation{variable.shared()}};
 }
 
 template <int N, int M, int O, typename OL, typename OR>
-auto matmul(ag::Variable<N, M, OL>& left, ag::Variable<M, O, OR>& right) {
+auto matmul(ag::Variable<N, M, OL> const& left, ag::Variable<M, O, OR> const& right) {
     using Operation = ag::operation::MatMul<N, M, O, OL, OR>;
     return ag::Variable<N, O, Operation>{Operation{left.shared(), right.shared()}};
 }
 
 template <int N, int M, typename OL, typename OR>
-auto operator+(ag::Variable<N, M, OL>& left, ag::Variable<N, M, OR>& right) {
+auto operator+(ag::Variable<N, M, OL> const& left, ag::Variable<N, M, OR> const& right) {
     using Operation = ag::operation::Plus<N, M, OL, OR>;
     return ag::Variable<N, M, Operation>{Operation{left.shared(), right.shared()}};
 }
 
 template <int N, int M, typename OL, typename OR>
-auto operator-(ag::Variable<N, M, OL>& left, ag::Variable<N, M, OR>& right) {
+auto operator-(ag::Variable<N, M, OL> const& left, ag::Variable<N, M, OR> const& right) {
     using Operation = ag::operation::Minus<N, M, OL, OR>;
     return ag::Variable<N, M, Operation>{Operation{left.shared(), right.shared()}};
 }
 
 template <int N, int M, typename OL, typename OR>
-auto operator*(ag::Variable<N, M, OL>& left, ag::Variable<N, M, OR>& right) {
+auto operator*(ag::Variable<N, M, OL> const& left, ag::Variable<N, M, OR> const& right) {
     using Operation = ag::operation::Mul<N, M, OL, OR>;
     return ag::Variable<N, M, Operation>{Operation{left.shared(), right.shared()}};
 }
--- tests.cpp
+++ tests.cpp
@@ -15,6 +15,50 @@ TEST_CASE("UseCases") {  // NOLINT
         c.zerograd();
         REQUIRE(a.grad().item() == 0.F);
     }
+
+    SECTION("FromReqs2") {
+        auto a = Scalar{3.F};
+        auto b = Scalar{4.F};
+        auto c = Scalar{5.F};
+        auto d = (a * b + c) - a;
+
+        REQUIRE(d.value().item() == 14.F);
+        d.backprop();
+        REQUIRE(a.grad().item() == 3.F);
+        REQUIRE(b.grad().item() == 3.F);
+        REQUIRE(c.grad().item() == 1.F);
+    }
+
+    SECTION("FromReqs3") {
+        auto a = Scalar{3.F};
+        auto b = Scalar{4.F};
+        auto c = a + b;
+        auto d = a * b;
+        auto e = c - d;
+
+        REQUIRE(e.value().item() == -5.F);
+        e.backprop();
+        REQUIRE(a.grad().item() == -3.F);
+        REQUIRE(b.grad().item() == -2.F);
+    }
+
+    SECTION("FromReqsFinal") {
+        auto weights = Matrix<2, 2>{1.F, 2.F, 3.F, 4.F};
+        auto bias = VectorRow<2>{5.F, 6.F};
+        auto x = VectorRow<2>{7.F, 8.F};
+        auto y = VectorRow<2>{9.F, 10.F};
+        auto y_pred = matmul(x, weights) + bias;
+        auto loss = avg((y - y_pred) * (y - y_pred));
+
+        REQUIRE(loss.value().item() == 1246.5);
+        loss.backprop();
+        REQUIRE(weights.grad().item(0, 0) == 189.F);
+        REQUIRE(weights.grad().item(0, 1) == 294.F);
+        REQUIRE(weights.grad().item(1, 0) == 216.F);
+        REQUIRE(weights.grad().item(1, 1) == 336.F);
+        REQUIRE(bias.grad().item(0) == 27.F);
+        REQUIRE(bias.grad().item(1) == 42.F);
+    }
 }
 
 TEST_CASE("Framework") {  // NOLINT
